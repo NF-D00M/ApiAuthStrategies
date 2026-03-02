4c7b1dd2abd50e5e3b2d7a7cf8fc9b5a709c9af7
galej <jack.gale@tabcorp.com.au>
2026-03-02 13:25:04 +1100

    git ignore

--- ApiAuthStrategies.Tests/obj/Debug/net9.0/ApiAuthStrategies.Tests.AssemblyInfo.cs
+++ ApiAuthStrategies.Tests/obj/Debug/net9.0/ApiAuthStrategies.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiAuthStrategies.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973b694690f7bc8c6bb89945877e58b59baa3c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiAuthStrategies.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiAuthStrategies.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
